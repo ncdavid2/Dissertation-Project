f1816059b81e81495b12f2b62c9b63b20cbca9cf
ncdavid2 <ncdavid@web.de>
2025-04-21 01:07:46 +0100

    star

--- server/api/graphql.js
+++ server/api/graphql.js
@@ -490,7 +490,7 @@ export default startServerAndCreateNextHandler(server, {
     }
   },
   cors: {
-    origin: 'https://dissertation-project-client.vercel.app/',
+    origin: '*',
     credentials: true,
   },
 });
