842aab25310560d56767f7d060fb87b4fbf01a39
ncdavid2 <ncdavid@web.de>
2025-05-01 13:02:32 +0100

    testing

--- server/utils/reminderAfterWeek.js
+++ server/utils/reminderAfterWeek.js
@@ -2,15 +2,17 @@ import cron from 'node-cron';
 import User from '../models/Users.js';
 import { sendLoginReminderEmail } from '../api/graphql.js';
 
+// '0 9 * * *' => '* * * * *' Makthe OneMinuteAgo work
 cron.schedule('0 9 * * *', async () => {
   console.log("Checking for inactive users...");
 
   const oneWeekAgo = new Date();
   oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
 
-  //This is to test if the email comes after 2 Minutes
+  //This is to test if the email comes after 1 Minutes
   //const oneMinuteAgo = new Date(Date.now() - 60 * 1000); // 1 minute ago
 
+  //change the oneWeekAgo to oneMinuteAgo for testing purposes
   const inactiveUsers = await User.find({
     lastLogin: {
       $lte: oneWeekAgo
